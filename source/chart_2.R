68a385221abe1ce06803c0620a736cd28b71918a
“adewan25” <avdew3@gmail.com>
2022-02-18 15:35:41 -0800

    final

--- source/chart_2.R
+++ source/chart_2.R
@@ -2,7 +2,16 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-source("/R Script aggregate.R")
+global_temp<-read.csv("../data/GlobalTemperatures.csv")
+
+
+surface_temperature <- global_temp %>%
+   mutate(dt = as.Date(dt, format = "%Y-%m-%d")) %>%
+   filter(dt >= as.Date("1958-01-01") & dt < as.Date("2019-01-01")) %>%
+   mutate(Year = format(dt, "%Y")) %>%
+   group_by(Year) %>%
+   summarize(AverageLandTemp = mean(LandAverageTemperature)) %>%
+   mutate(Year = as.numeric(Year))
 
 surface_temp_chart <- ggplot(surface_temperature, aes(x = Year, y = AverageLandTemp)) + 
       geom_point() + 
