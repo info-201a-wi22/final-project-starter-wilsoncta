eb2957471df763e7c3d3d5f8dca1df14f965c7ff
“adewan25” <avdew3@gmail.com>
2022-02-18 16:14:43 -0800

    final versin lat left

--- source/chart_3.R
+++ source/chart_3.R
@@ -2,7 +2,11 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-source("../source/R Script aggregate.R")
+Carbon_levels_data<-read.csv("../data/CarbonLevel.csv")
+agre4<-Carbon_levels_data%>%
+  group_by(Year)%>%
+  filter(Seasonally.Adjusted.CO2.Fit..ppm.==max(Seasonally.Adjusted.CO2.Fit..ppm., na.rm= TRUE))%>%
+  select(Year,Seasonally.Adjusted.CO2.Fit..ppm.)
 
 year_graph <- ggplot(agre4, aes(x= Year, y = Seasonally.Adjusted.CO2.Fit..ppm.)) +
   geom_col() +
