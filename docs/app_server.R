dc42aac1670327fe67b9c6e9003b62ce6b39346a
“adewan25” <avdew3@gmail.com>
2022-03-06 17:15:52 -0800

    chart added

--- docs/app_server.R
+++ docs/app_server.R
@@ -1,8 +1,49 @@
 library(knitr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
 
+global_temp<-read.csv("/Users/vivekdewangan/Desktop/info201codes/final-project-starter-wilsoncta/data/GlobalTemperatures.csv")
+
+Carbon_levels_data<-read.csv("/Users/vivekdewangan/Desktop/info201codes/final-project-starter-wilsoncta/data/CarbonLevel.csv")
+
+build_chart <- function(radio,plot) {
+  
+  surface_temperature <- global_temp %>%
+    mutate(dt = as.Date(dt, format = "%Y-%m-%d")) %>%
+    filter(dt >= as.Date("1958-01-01") & dt < as.Date("2019-01-01")) %>%
+    mutate(Year = format(dt, "%Y")) %>%
+    group_by(Year) %>%
+    summarize(AverageLandTemp = mean(LandAverageTemperature)) %>%
+    mutate(Year = as.numeric(Year)) %>% 
+    filter(Year>=radio) %>% 
+    select(Year,AverageLandTemp)
+  
+  
+  # Plot data
+  p <- plot_ly(
+    data = surface_temperature,
+    x = ~surface_temperature$Year, 
+    y = ~surface_temperature$AverageLandTemp, 
+    type = plot, 
+    mode = "markers"
+    
+  ) %>%
+    layout(
+      title = "Trends of Average Land Temperature",                  # plot title
+      xaxis = list(title = "Year"), # axis label + format
+      yaxis = list(title = " Average Land Temperature (Celcius)")  # axis label + format
+    )
+  return(p)
+}
 
 server<-server <- function(input, output) { 
   output$markdown <- renderUI({
     HTML(markdown::markdownToHTML(knit('introduction.Rmd', quiet = TRUE)))
   })
+  
+  output$chart <- renderPlotly({
+    return(build_chart(input$radio,input$plot))
+  })
+  
 }
--- docs/app_ui.R
+++ docs/app_ui.R
@@ -23,11 +23,60 @@ page_one <- tabPanel(
   )
  )
 
+chart_sidebar_content <- sidebarPanel(
+ 
+  radioButtons("radio", label = h3("Year"),
+               choices =  c("2012","2011","2010","2009","2008","2007","2006") ,
+               selected = "2012"
+  ),
+  
+  
+  
+  
+  checkboxGroupInput("plot", label = h3("Type of Plot"), 
+                     choices = c("bar","scatter"),
+                     selected = "bar")
+  
+)
+
+
+
+chart_main_content <- mainPanel(
+  plotlyOutput("chart"),
+  
+  p("
+We have used scatter plot and bar bargraph with a fitting line to depict how average 
+land temperature has varied from 2006 to 2015. 
+Specefically the x-axis is depicting the years and the y-axis 
+is depicting the average land temperature (in Celsius). 
+ Climate change has led to a massive increase in average land temperature 
+over the years which is not at all good for the inhabitants of the planet earth.  ")
+)
+
+chart_panel <- tabPanel(
+  "Chart 1",
+  
+  # Add a titlePanel to your tab
+  titlePanel("Chart 1"),
+  
+  # Create a sidebar layout for this tab (page)
+  sidebarLayout(
+    
+    # Display your `scatter_sidebar_content`
+    chart_sidebar_content,
+    
+    # Display your `scatter_main_content`
+    chart_main_content
+  )
+)
+
+
 
   
 ui <- navbarPage(
   "Catastrophic Effects of Climate Change",
-  page_one
+  page_one,
+  chart_panel
   
 )
 
